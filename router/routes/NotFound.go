23e7d3263df8eea671f9bad69d21aefdf5d2af6b
airbound <98663009+airbound@users.noreply.github.com>
2022-03-06 16:06:37 -0500

    feat: Attempt to show the path to a valid route when route is not found.

--- router/routes/NotFound.go
+++ router/routes/NotFound.go
@@ -9,6 +9,6 @@ func NotFound(ctx *fiber.Ctx) error {
 	return ctx.Status(404).JSON(fiber.Map{
 		"status":  "ERROR",
 		"errors":  structs.Errors{"ROUTE_NOT_FOUND"},
-		"message": "This route could not be found. Please provide a valid route and try again.",
+		"message": "This route could not be found. Please provide a valid route and try again. (Maybe you mean /v2" + ctx.Path() + "?)",
 	})
 }
