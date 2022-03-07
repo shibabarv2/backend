959d4d369d50eb8e87bf1b4054c47cf5a9569ff4
airbound <98663009+airbound@users.noreply.github.com>
2022-03-07 16:34:20 -0500

    refactor: remove autocompletion route when user provides a /v2/ path

--- router/routes/NotFound.go
+++ router/routes/NotFound.go
@@ -12,7 +12,7 @@ func NotFound(ctx *fiber.Ctx) error {
 		return ctx.Status(fiber.StatusNotFound).JSON(fiber.Map{
 			"status":  "ERROR",
 			"errors":  structs.Errors{"ROUTE_NOT_FOUND"},
-			"message": "This route could not be found. Please provide a valid route and try again. (Maybe you mean /" + ctx.Path() + "?)",
+			"message": "This route could not be found. Please provide a valid route and try again",
 		})
 	}
 
